d0fcd4d5493279e5af4f40c3bc4e501dba5bd3df
Azamosa Harry <johnharry238@gmail.com>
2023-11-03 12:17:20 +0100

    commit

--- src/Writer.js
+++ src/Writer.js
@@ -1,9 +1,55 @@
+import { useEffect, useRef, useState } from 'react'
+import Footer from './Footer'
+import Header from './Header'
 import './Writer.css'
 
 function Writer () {
-    return (
-        <div>
+    const containerRef1 = useRef(null)
 
+    const [isVisible, setIsVisible] = useState(false)
+    const [loaded1, setLoaded1] = useState(false)
+
+    const callbackFunction1 = (entries) => {
+        const [ entry ] = entries
+        setLoaded1(entry.isIntersecting)
+        if (loaded1 == true) {
+            setIsVisible(true)
+        }
+    }
+    const options1 = {
+        root: null,
+        rootMargin: "0px",
+        threshold: 0.5
+    }
+
+    useEffect(() => {
+        const observer1 = new IntersectionObserver(callbackFunction1, options1)
+        if (containerRef1.current) observer1.observe (containerRef1.current)
+    return () => {
+        if (containerRef1.current) observer1.unobserve(containerRef1.current)
+    }
+    }, [containerRef1, options1])
+
+    return (
+        <div className='writer'>
+            <div style={{minHeight: '1px'}}>
+
+            </div>
+            <Header />
+            <div className='writer_body'>
+                <section ref={containerRef1} className={loaded1? 'writer_section_one visible-bg':'writer_section_one'}>
+                    <div className='writer_section_one_div1'>
+                        <h1>"For what is life without her?"</h1>
+                        <h1>"being without Saphira is like unto death,"</h1>
+                        <h1>"but less bearable"</h1>
+                        <p>Except from the humming bird</p>
+                    </div>
+                </section>
+                <section>
+
+                </section>
+            </div>
+            <Footer />
         </div>
     )
 }
