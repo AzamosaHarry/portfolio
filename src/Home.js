a1a21827bba039aab927499428efd7d92afd29a3
Azamosa Harry <johnharry238@gmail.com>
2023-11-16 12:44:41 +0100

    commit

--- src/Home.js
+++ src/Home.js
@@ -15,46 +15,46 @@ function Home() {
                 <div className='section_two_bg'>
                     <div className='section_two_fg'>
                       <div className='primary_one'>
-                        <h1>Azamosa Harry</h1>
+                        <h1>If you know me, you will know my story</h1>
                         <BsSuitDiamondFill />   
-                        <h1>Azamosa Harry</h1>
+                        <h1>If you know me, you will know my story</h1>
                         <BsSuitDiamondFill />   
-                        <h1>Azamosa Harry</h1>
+                        <h1>If you know me, you will know my story</h1>
                         <BsSuitDiamondFill />   
-                        <h1>Azamosa Harry</h1>
+                        <h1>If you know me, you will know my story</h1>
                         <BsSuitDiamondFill />   
-                        <h1>Azamosa Harry</h1>
+                        <h1>If you know me, you will know my story</h1>
                         <BsSuitDiamondFill />   
-                        <h1>Azamosa Harry</h1>
+                        <h1>If you know me, you will know my story</h1>
                         <BsSuitDiamondFill />   
-                        <h1>Azamosa Harry</h1>
+                        <h1>If you know me, you will know my story</h1>
                         <BsSuitDiamondFill />   
-                        <h1>Azamosa Harry</h1>
+                        <h1>If you know me, you will know my story</h1>
                       </div>
                       <div className='primary_two'>
-                        <h1>Azamosa Harry</h1>
+                        <h1>If you know me, you will know my story</h1>
                         <BsSuitDiamondFill />   
-                        <h1>Azamosa Harry</h1>
+                        <h1>If you know me, you will know my story</h1>
                         <BsSuitDiamondFill />   
-                        <h1>Azamosa Harry</h1>
+                        <h1>If you know me, you will know my story</h1>
                         <BsSuitDiamondFill />   
-                        <h1>Azamosa Harry</h1>
+                        <h1>If you know me, you will know my story</h1>
                         <BsSuitDiamondFill />   
-                        <h1>Azamosa Harry</h1>
+                        <h1>If you know me, you will know my story</h1>
                         <BsSuitDiamondFill />   
-                        <h1>Azamosa Harry</h1>
+                        <h1>If you know me, you will know my story</h1>
                         <BsSuitDiamondFill />   
-                        <h1>Azamosa Harry</h1>
+                        <h1>If you know me, you will know my story</h1>
                         <BsSuitDiamondFill />   
-                        <h1>Azamosa Harry</h1>
+                        <h1>If you know me, you will know my story</h1>
                       </div>
                       
                     </div>
                   </div>
                   <div className='home_section_one_hero'>
-                    <h1>If you know me, you will know my story</h1>
+                    <h1>Just call me Harry</h1>
                     <br />
-                    <p>My name is Azamosa Harry and I am a <span style={{color: 'gray'}}>beautiful mosaic</span> of lived experience and culture.</p>
+                    <p>My name is  <span style={{color: 'lemonchiffon'}}> AZAMOSA HARRY</span> and I am a beautiful mosaic of lived experience and culture.</p>
                     <br />
                     <p>I am a Mechanical engineer, Software developer, designer and writer. Yes, I enjoy the diversity of knowledge!</p>
                   </div>
@@ -67,11 +67,23 @@ function Home() {
                 <section className='home_section_two'>
                     <p>While you are here,</p>
                     <h1>Go through my respective portfolios</h1>
-                    <div>
-                        <button onClick={() => {navigate('/portfolio/engineering')}}>Mechanical engineering</button>
-                        <button onClick={() => {navigate('/portfolio/developer')}}>Software developing</button>
-                        <button onClick={() => {navigate('/portfolio/design')}}>Design</button>
-                        <button onClick={() => {navigate('/portfolio/writer')}}>Writer</button>
+                    <div className='home_section_two_div'>
+                      <div onClick={() => {navigate('/portfolio/engineering')}}>
+                          <h1>Mechanical engineering</h1>
+                          <h3>SUBMIT TICKET</h3>
+                      </div>
+                      <div onClick={() => {navigate('/portfolio/developer')}}>
+                          <h1>Software developing</h1>
+                          <h3>SUBMIT TICKET</h3>
+                      </div>
+                      <div onClick={() => {navigate('/portfolio/design')}}>
+                          <h1>Design</h1>
+                          <h3>SUBMIT TICKET</h3>
+                      </div>
+                      <div  onClick={() => {navigate('/portfolio/writer')}}>
+                          <h1>Writer</h1>
+                          <h3>SUBMIT TICKET</h3>
+                      </div>
                     </div>
                 </section>
             </div>
--- src/Insight.js
+++ src/Insight.js
@@ -7,7 +7,178 @@ function Insight () {
         <div className='insight'>
             <Header />
             <div className='insight_body'>
-
+                <section className='insight_section_one'>
+                    <div>
+                        <h3>My Online CV</h3>
+                        <h1>My career so far.</h1>
+                    </div>
+                </section>
+                <section className='insight_section_two'>
+                    <div className='insight_section_two_div1'>
+                        <h1>Work Experience.</h1>
+                    </div>
+                    <div className='insight_section_two_div2'>
+                        <div>
+                            <h3>NYSC ENGINNERING CRAINEE</h3>
+                            <h1><span style={{color: 'burlywood'}}>Chibufavor Automobile,</span> Umuahia, Abia, August 2023 - Present</h1>
+                            <p>Cillum quis nulla ea duis est est aliqua aliquip laboris commodo proident. Proident id nulla Lorem duis et ex duis ex esse dolor magna. Exercitation pariatur amet ut eu amet irure dolor consequat. Consectetur sunt cupidatat officia deserunt.</p>
+                        </div>
+                        <div>
+                            <h3>LEAD DEVELOPER AND UI/UX DESIGNER</h3>
+                            <h1><span style={{color: 'burlywood'}}>Projecf group,</span> Umuahia, Abia, August 2023 - Present</h1>
+                            <p>Cillum quis nulla ea duis est est aliqua aliquip laboris commodo proident. Proident id nulla Lorem duis et ex duis ex esse dolor magna. Exercitation pariatur amet ut eu amet irure dolor consequat. Consectetur sunt cupidatat officia deserunt.</p>
+                        </div>
+                        <div>
+                            <h3>CHIEF EDIFOR</h3>
+                            <h1><span style={{color: 'burlywood'}}>Mechanical engineering journal,</span> Universiy of Benin, Benin, August 2023 - Present</h1>
+                            <p>Cillum quis nulla ea duis est est aliqua aliquip laboris commodo proident. Proident id nulla Lorem duis et ex duis ex esse dolor magna. Exercitation pariatur amet ut eu amet irure dolor consequat. Consectetur sunt cupidatat officia deserunt.</p>
+                        </div>
+                        <div>
+                            <h3>NYSC Engineering trainee</h3>
+                            <h1><span>Chibufavor Automobile</span> Umuahia, Abia, August 2023 - Present</h1>
+                            <p>Cillum quis nulla ea duis est est aliqua aliquip laboris commodo proident. Proident id nulla Lorem duis et ex duis ex esse dolor magna. Exercitation pariatur amet ut eu amet irure dolor consequat. Consectetur sunt cupidatat officia deserunt.</p>
+                        </div>
+                        <div>
+                            <h3>NYSC Engineering trainee</h3>
+                            <h1><span>Chibufavor Automobile</span> Umuahia, Abia, August 2023 - Present</h1>
+                            <p>Cillum quis nulla ea duis est est aliqua aliquip laboris commodo proident. Proident id nulla Lorem duis et ex duis ex esse dolor magna. Exercitation pariatur amet ut eu amet irure dolor consequat. Consectetur sunt cupidatat officia deserunt.</p>
+                        </div>
+                        <div>
+                            <h3>NYSC Engineering trainee</h3>
+                            <h1><span>Chibufavor Automobile</span> Umuahia, Abia, August 2023 - Present</h1>
+                            <p>Cillum quis nulla ea duis est est aliqua aliquip laboris commodo proident. Proident id nulla Lorem duis et ex duis ex esse dolor magna. Exercitation pariatur amet ut eu amet irure dolor consequat. Consectetur sunt cupidatat officia deserunt.</p>
+                        </div>
+                        <div>
+                            <h3>NYSC Engineering trainee</h3>
+                            <h1><span>Chibufavor Automobile</span> Umuahia, Abia, August 2023 - Present</h1>
+                            <p>Cillum quis nulla ea duis est est aliqua aliquip laboris commodo proident. Proident id nulla Lorem duis et ex duis ex esse dolor magna. Exercitation pariatur amet ut eu amet irure dolor consequat. Consectetur sunt cupidatat officia deserunt.</p>
+                        </div>
+                        <div>
+                            <h3>NYSC Engineering trainee</h3>
+                            <h1><span>Chibufavor Automobile</span> Umuahia, Abia, August 2023 - Present</h1>
+                            <p>Cillum quis nulla ea duis est est aliqua aliquip laboris commodo proident. Proident id nulla Lorem duis et ex duis ex esse dolor magna. Exercitation pariatur amet ut eu amet irure dolor consequat. Consectetur sunt cupidatat officia deserunt.</p>
+                        </div>
+                    </div>
+                </section>
+                <section className='insight_section_two'>
+                    <div className='insight_section_two_div1'>
+                        <h1>Education.</h1>
+                    </div>
+                    <div className='insight_section_two_div2'>
+                        <div>
+                            <h3>BACHELOR IN ENGINEERING, MECHANICAL ENGINEERING.</h3>
+                            <h1><span style={{color: 'burlywood'}}>University of Benin,</span> Benin City, Edo State</h1>
+                            <p>December 2022</p>
+                        </div>
+                        <div>
+                            <h3>HIGH SCHOOL DIPLOMA</h3>
+                            <h1><span style={{color: 'burlywood'}}>School of secondary educaion, I.C.E.</span> Warri, Delta state</h1>
+                            <p> July 2013</p>
+                        </div>
+                    </div>
+                </section>
+                <section className='insight_section_two'>
+                    <div className='insight_section_two_div1'>
+                        <h1>General Skills.</h1>
+                    </div>
+                    <div className='insight_section_two_div2'>
+                        <div>
+                            <h1>development skills.</h1>
+                            <h3><span style={{color: 'burlywood'}}>Our growth international,</span></h3>
+                            <p>issued: October 2023</p>
+                        </div>
+                        <div>
+                            <h3>engineering skills</h3>
+                            <h1><span style={{color: 'burlywood'}}>british certificaion inc.</span></h1>
+                            <p>issued: October 2023</p>
+                        </div>
+                    </div>
+                </section>
+                <section className='insight_section_two'>
+                    <div className='insight_section_two_div1'>
+                        <h1>Lincences and Cerificacion.</h1>
+                    </div>
+                    <div className='insight_section_two_div2'>
+                        <div>
+                            <h1>Cerified Professional Project manager.</h1>
+                            <h3><span style={{color: 'burlywood'}}>Our growth international,</span></h3>
+                            <p>issued: October 2023</p>
+                        </div>
+                        <div>
+                            <h3>HUMAN RESOURCE MANAGEMEN</h3>
+                            <h1><span style={{color: 'burlywood'}}>british certificaion inc.</span></h1>
+                            <p>issued: October 2023</p>
+                        </div>
+                    </div>
+                </section>
+                <section className='insight_section_two'>
+                    <div className='insight_section_two_div1'>
+                        <h1>Membership and organizaions.</h1>
+                    </div>
+                    <div className='insight_section_two_div2'>
+                        <div>
+                            <h1>Cerified Professional Project manager.</h1>
+                            <h3><span style={{color: 'burlywood'}}>Our growth international,</span></h3>
+                            <p>issued: October 2023</p>
+                        </div>
+                        <div>
+                            <h3>HUMAN RESOURCE MANAGEMEN</h3>
+                            <h1><span style={{color: 'burlywood'}}>british certificaion inc.</span></h1>
+                            <p>issued: October 2023</p>
+                        </div>
+                    </div>
+                </section>
+                <section className='insight_section_two'>
+                    <div className='insight_section_two_div1'>
+                        <h1>Honors and awards.</h1>
+                    </div>
+                    <div className='insight_section_two_div2'>
+                        <div>
+                            <h1>Cerified Professional Project manager.</h1>
+                            <h3><span style={{color: 'burlywood'}}>Our growth international,</span></h3>
+                            <p>issued: October 2023</p>
+                        </div>
+                        <div>
+                            <h3>HUMAN RESOURCE MANAGEMEN</h3>
+                            <h1><span style={{color: 'burlywood'}}>british certificaion inc.</span></h1>
+                            <p>issued: October 2023</p>
+                        </div>
+                    </div>
+                </section>
+                <section className='insight_section_two'>
+                    <div className='insight_section_two_div1'>
+                        <h1>publications and research.</h1>
+                    </div>
+                    <div className='insight_section_two_div2'>
+                        <div>
+                            <h1>Cerified Professional Project manager.</h1>
+                            <h3><span style={{color: 'burlywood'}}>Our growth international,</span></h3>
+                            <p>issued: October 2023</p>
+                        </div>
+                        <div>
+                            <h3>HUMAN RESOURCE MANAGEMEN</h3>
+                            <h1><span style={{color: 'burlywood'}}>british certificaion inc.</span></h1>
+                            <p>issued: October 2023</p>
+                        </div>
+                    </div>
+                </section>
+                <section className='insight_section_two'>
+                    <div className='insight_section_two_div1'>
+                        <h1>voluneering.</h1>
+                    </div>
+                    <div className='insight_section_two_div2'>
+                        <div>
+                            <h1>Cerified Professional Project manager.</h1>
+                            <h3><span style={{color: 'burlywood'}}>Our growth international,</span></h3>
+                            <p>issued: October 2023</p>
+                        </div>
+                        <div>
+                            <h3>HUMAN RESOURCE MANAGEMEN</h3>
+                            <h1><span style={{color: 'burlywood'}}>british certificaion inc.</span></h1>
+                            <p>issued: October 2023</p>
+                        </div>
+                    </div>
+                </section>
             </div>
             <Footer />
         </div>
