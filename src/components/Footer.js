531fa6fa745008357c08b40ddaa9a217b5f9a4ef
Azamosa Harry <johnharry238@gmail.com>
2023-12-27 14:41:21 +0100

    commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,27 @@
 import { Link, useNavigate } from 'react-router-dom'
 import cv from '../assets/AzamosaHarryOsawayemen.pdf'
+import logowhite from '../assets/logo-white.png' 
 import './Footer.css'
 
 function Footer() {
     const navigate = useNavigate()
+
+    function notification() {
+        Notification.requestPermission().then((permission) => {
+            if (permission == 'granted') {
+                new Notification('Azamosa Harry Portfolio', {
+                    body: `The curriculum vitae of Azamosa Harry Osawayemen has been downloaded to your local device storage`,
+                    icon: {logowhite},
+                    image: {logowhite}
+                })
+            }
+        })
+    }    
+
     return (
         <div className='footer'>
             <div>
-                <a href={cv} download target='_blank'><p>Download CV</p></a>
+                <a href={cv} download target='_blank' onClick={notification}><p>Download CV</p></a>
                 <h3>|</h3>
                 <p onClick={() => window.location = 'mailto:johnharry@gmail.com?subject=Access request'} >Johnharry238@gmail.com</p>
                 <h3>|</h3>
